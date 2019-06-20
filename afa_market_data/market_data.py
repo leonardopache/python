72ebace14d11f281609b9d217327d22c9386969a
Leonardo <leonardo@pache.eng.br>
2019-06-20 16:34:20 +0200

    totally dynamic with files

--- afa_market_data/market_data.py
+++ afa_market_data/market_data.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from afa_market_data.market_data import ManagerREIT, reit_custom, ManageFileUtil, ReadPagesUtil, FII_CVM_CAD_URL, \
     BMF_SERIES_HIST_YEAR
 
+file_name = 'COTAHIST_A2019.TXT'
 
 class MarketData:
 
@@ -14,14 +15,24 @@ class MarketData:
     def download_files_daily():
         """
             Execute function to download updated list of Real Estate Funds
+            and information about last market trader
         :return:
             None
         """
+        # FII information
         last_cvs_file = ReadPagesUtil.load_table_FI_cadastre(FII_CVM_CAD_URL)
-
         # for the latest row download url + column name
         # CSV file is downloaded and if valid file swap with actual inf_cadastral_fie.csv
         ManageFileUtil.download_file(FII_CVM_CAD_URL + last_cvs_file, 'inf_cadastral_fie.csv')
+
+        # trader information
+        ManageFileUtil.download_file(BMF_SERIES_HIST_YEAR, 'file.zip')
+
+        # if contains file_name in folder
+        # rename to old
+        ManageFileUtil.unzip_file('file.zip')
+        # if contains file_name in folder
+        # delete file_name old and zip file
 
     @staticmethod
     def update_reit_cad_information():
@@ -33,16 +44,7 @@ class MarketData:
 
     # function to update daily information from REIT's
     @staticmethod
-    def update_last_daily_price():
-        ManageFileUtil.download_file(BMF_SERIES_HIST_YEAR, 'file.zip')
-
-        file_name = 'COTAHIST_A2019.TXT'
-        # if contains file_name in folde
-        # rename to old
-        ManageFileUtil.unzip_file('file.zip')
-        # if contains file_name in folder
-        # delete file_name old and zip file
-
+    def update_reit_last_daily_price():
         ManagerREIT.collect_REIT_daily_stock(file_name)
 
     @staticmethod
@@ -55,17 +57,24 @@ if __name__ == '__main__':
     print(datetime.now())
     try:
         # todos os dias uteis depois das 7
-        #MarketData.download_files_daily()
+        MarketData.download_files_daily()
 
         # monthly
         #MarketData.update_reit_cad_information()
 
         # sempre que for avaliar os fundos
-        MarketData.update_last_daily_price()
+        MarketData.update_reit_last_daily_price()
         MarketData.run_reits_custom_analisys()
 
-        # send cvs to google drive
+        ''' Stock market '''
+        # update list of CIA opened from CMV
 
+        # load basic info for each CIA from BMF (CNPJ, Ticker, ¿list of dividends?)
+
+        # update with historic market values
+
+
+        # send cvs to google drive
         print(datetime.now())
     except Exception as e:
         print(e)
--- afa_market_data/market_data/constants.py
+++ afa_market_data/market_data/constants.py
@@ -2,23 +2,26 @@
 # -*- coding: utf-8 -*-
 # list os constant links to consult values
 FILE_PATH = 'market_data/files/'
+BMF_URL_BASE = 'http://bvmf.bmfbovespa.com.br/'
 
 TESOURO_DIRETO_TITULO_TAX = 'http://www.tesouro.fazenda.gov.br/tesouro-direto-precos-e-taxas-dos-titulos'
 
 GOOGLE_SHEETS_HOME = 'https://spreadsheets.google.com/feeds'
 GOOGLE_DRIVE_AUTH = 'https://www.googleapis.com/auth/drive'
 
-FII_BMF_URL_BASE = 'http://bvmf.bmfbovespa.com.br/Fundos-Listados/'
-FII_BMF_LIST_ALL = 'FundosListados.aspx?tipoFundo=imobiliario&Idioma=pt-br'
-#FII_BMF_DETAIL_TAB = 'FundosListadosDetalhe.aspx?Sigla={}&tipoFundo=Imobiliario&aba=abaPrincipal&idioma=en-us'
-
 YAHOO_FINANCE_TICKER_HISTORY = 'https://finance.yahoo.com/quote/{0}.SA/history?p={0}.SA&.tsrc=fin-srch'
 
-FII_BMF_EVENTS_TAB = 'FundosListadosDetalhe.aspx?Sigla={}&tipoFundo=Imobiliario&aba=abaEventosCorporativos&idioma=en-us'
 FII_CVM_BASE = 'http://fnet.bmfbovespa.com.br/fnet/publico/'
 FII_CVM_DOCS_LIST = 'abrirGerenciadorDocumentosCVM?cnpjFundo={}'
-
 FII_CVM_CAD_URL = 'http://dados.cvm.gov.br/dados/FIE/CAD/DADOS/'
 
-BMF_SERIES_HIST_YEAR = 'http://bvmf.bmfbovespa.com.br/InstDados/SerHist/COTAHIST_A2019.ZIP'
+FII_BMF_EVENTS_TAB = 'FundosListadosDetalhe.aspx?Sigla={}&tipoFundo=Imobiliario&aba=abaEventosCorporativos&idioma=en-us'
+FII_BMF_URL_BASE = BMF_URL_BASE+'Fundos-Listados/'
+FII_BMF_LIST_ALL = 'FundosListados.aspx?tipoFundo=imobiliario&Idioma=pt-br'
 
+BMF_SERIES_HIST_YEAR = BMF_URL_BASE+'InstDados/SerHist/COTAHIST_A2019.ZIP'
+
+BMF_CIA_INF_BASE_URL = BMF_URL_BASE+'cias-Listadas/Empresas-Listadas/'
+BMF_CIA_TAB_RESUME = 'ResumoEmpresaPrincipal.aspx?codigoCvm={}&idioma=pt-br'
+
+CIA_CVM_CAD_URL = 'http://dados.cvm.gov.br/dados/CIA_ABERTA/CAD/DADOS/'
